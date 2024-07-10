bce93baca29c059e2b90d9de0fa874df17c808ca
jspreeti <pyadav2007@gmail.com>
2024-07-10 12:20:34 -0400

    Adding Address Project uses feign client

---
+++ address-service/src/test/java/com/binarylogicit/address/AddressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarylogicit.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
